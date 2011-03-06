5c8fa27bc52ecec90ba79dc1cf0cd79bc8dd1650
Jon Indig <zimasprophet@gmail.com>
2011-03-05 19:25:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DietApp::Application.config.secret_token = '12ea203576a083bc321c01e98809367e4453300c87236d3e4ace74cb818b1da6cc2226a940771b6dac03b3a342b94642d9cce8b1178c7c39a163699f1f00e67b'
