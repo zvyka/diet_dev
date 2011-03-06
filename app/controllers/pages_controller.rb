e714c6ad6ac1648198699deb40b0f0fbdc04e069
Jon Indig <zimasprophet@gmail.com>
2011-03-05 20:16:59 -0500

    done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,42 @@
 class PagesController < ApplicationController
+
   def home
+    @title = "Home"
+    if signed_in?
+      @micropost = Micropost.new
+      @feed_items = current_user.feed.paginate(:page => params[:page])
+    end
   end
 
   def contact
+    @title = "Contact"
   end
 
   def about
+    @title = "About"
   end
-
+  
   def help
+    @title = "Help"
   end
-
-  def faq
+  
+  def microposts
+    @title = "Microposts"
   end
-
-  def privacy
+  
+  def analysis
+    @title = "Analysis"
   end
-
+  
   def terms
+    @title = "Terms and Conditions"
   end
-
+  
+  def privacy
+    @title = "Privacy Policy"
+  end
+  
+  def faq
+    @title = "Frequently Asked Questions"
+  end
 end
