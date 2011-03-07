da808022cc4a7218c12f52998f4efe07aaa3bf87
Jon Indig <zimasprophet@gmail.com>
2011-03-06 22:32:29 -0500

    User signup done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,20 +52,6 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
   
-  def following
-    @title = "Following"
-    @user = User.find(params[:id])
-    @users = @user.following.paginate(:page => params[:page])
-    render 'show_follow'
-  end
-
-  def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(:page => params[:page])
-    render 'show_follow'
-  end
-  
   private
 
     def correct_user
